60f02628fc00cdaf24acd5fcbd9165de99bdd4f3
Steve Clay <steve@mrclay.org>
2014-04-15 18:31:42 -0400

    Rename lists in test

--- tests/ElggxListsTest.php
+++ tests/ElggxListsTest.php
@@ -15,13 +15,13 @@ class ElggxListsTest extends ElggCoreUnitTest {
 	public function testGetListHandlesEntitiesAndInts() {
 		$site = elgg_get_site_entity();
 
-		$coll = elggx_get_list($site);
-		$this->assertIsA($coll, 'Elggx_Lists_List');
-		$this->assertEqual($coll->getEntityGuid(), $site->guid);
+		$list = elggx_get_list($site);
+		$this->assertIsA($list, 'Elggx_Lists_List');
+		$this->assertEqual($list->getEntityGuid(), $site->guid);
 
-		$coll = elggx_get_list($site->guid);
-		$this->assertIsA($coll, 'Elggx_Lists_List');
-		$this->assertEqual($coll->getEntityGuid(), $site->guid);
+		$list = elggx_get_list($site->guid);
+		$this->assertIsA($list, 'Elggx_Lists_List');
+		$this->assertEqual($list->getEntityGuid(), $site->guid);
 	}
 
 	public function testEntityDeleteEmptiesLists() {
@@ -30,30 +30,30 @@ class ElggxListsTest extends ElggCoreUnitTest {
 
 		$name = 'testDeleteEmpties';
 
-		$coll = elggx_get_list($obj, $name);
-		$coll->push(elgg_get_site_entity());
+		$list = elggx_get_list($obj, $name);
+		$list->push(elgg_get_site_entity());
 
-		$this->assertEqual($coll->count(), 1);
+		$this->assertEqual($list->count(), 1);
 
 		$obj->delete();
 
-		$this->assertEqual($coll->count(), 0);
+		$this->assertEqual($list->count(), 0);
 	}
 
 	public function testReverseLookups() {
 		$false_entity = 3;
 		$searched_entity = 2;
 
-		$test_colls = array();
+		$test_lists = array();
 		foreach (range(0, 9) as $i) {
-			$coll = elggx_get_list($i, "testReverseLookups{$i}");
-			$coll->push($false_entity);
+			$list = elggx_get_list($i, "testReverseLookups{$i}");
+			$list->push($false_entity);
 			if ($i % 2 == 0) {
-				$coll->push($searched_entity);
+				$list->push($searched_entity);
 			}
-			$test_colls[] = $coll;
+			$test_lists[] = $list;
 		}
-		/* @var Elggx_Lists_List[] $test_colls */
+		/* @var Elggx_Lists_List[] $test_lists */
 
 		$count = elggx_get_containing_lists($searched_entity, array('count' => true));
 		$this->assertEqual($count, 5);
@@ -63,22 +63,22 @@ class ElggxListsTest extends ElggCoreUnitTest {
 		foreach (range(0, 9, 2) as $i) {
 			$expected["{$i}:testReverseLookups{$i}"] = true;
 		}
-		$found_colls = elggx_get_containing_lists($searched_entity);
-		foreach ($found_colls as $coll) {
-			$key = $coll->getEntityGuid() . ":" . $coll->getName();
+		$found_lists = elggx_get_containing_lists($searched_entity);
+		foreach ($found_lists as $list) {
+			$key = $list->getEntityGuid() . ":" . $list->getName();
 			unset($expected[$key]);
 		}
 		$this->assertEqual(count($expected), 0);
 
-		$colls = elggx_get_containing_lists($searched_entity, array('limit' => 3, 'offset' => 0));
-		$this->assertEqual(count($colls), 3);
-		$colls = elggx_get_containing_lists($searched_entity, array('limit' => 3, 'offset' => 3));
-		$this->assertEqual(count($colls), 2);
-		$colls = elggx_get_containing_lists($searched_entity, array('limit' => 3, 'offset' => 6));
-		$this->assertEqual(count($colls), 0);
+		$lists = elggx_get_containing_lists($searched_entity, array('limit' => 3, 'offset' => 0));
+		$this->assertEqual(count($lists), 3);
+		$lists = elggx_get_containing_lists($searched_entity, array('limit' => 3, 'offset' => 3));
+		$this->assertEqual(count($lists), 2);
+		$lists = elggx_get_containing_lists($searched_entity, array('limit' => 3, 'offset' => 6));
+		$this->assertEqual(count($lists), 0);
 
-		foreach ($test_colls as $coll) {
-			$coll->removeAll();
+		foreach ($test_lists as $list) {
+			$list->removeAll();
 		}
 	}
 
@@ -88,43 +88,43 @@ class ElggxListsTest extends ElggCoreUnitTest {
 	public function testItemAccess() {
 		$user = elgg_get_logged_in_user_entity();
 		$name = 'test_list';
-		$coll = elggx_get_list($user, $name);
+		$list = elggx_get_list($user, $name);
 
-		$this->assertEqual($coll->count(), 0);
+		$this->assertEqual($list->count(), 0);
 
-		$coll->push(1);
-		$this->assertEqual($coll->count(), 1);
-		$this->assertTrue($coll->hasAnyOf(1));
+		$list->push(1);
+		$this->assertEqual($list->count(), 1);
+		$this->assertTrue($list->hasAnyOf(1));
 
-		$coll->push(array(2, 3, 1, $user));
-		$this->assertEqual($coll->count(), 4);
-		$this->assertTrue($coll->hasAnyOf($user));
-		$this->assertTrue($coll->hasAnyOf($user->guid));
+		$list->push(array(2, 3, 1, $user));
+		$this->assertEqual($list->count(), 4);
+		$this->assertTrue($list->hasAnyOf($user));
+		$this->assertTrue($list->hasAnyOf($user->guid));
 
-		$this->assertEqual($coll->indexOf($user), 3);
-		$this->assertFalse($coll->indexOf($user->guid + 5));
+		$this->assertEqual($list->indexOf($user), 3);
+		$this->assertFalse($list->indexOf($user->guid + 5));
 
-		$coll->remove(array($user, 1));
-		$this->assertEqual($coll->count(), 2);
+		$list->remove(array($user, 1));
+		$this->assertEqual($list->count(), 2);
 
-		$coll->removeAll();
-		$this->assertEqual($coll->count(), 0);
+		$list->removeAll();
+		$this->assertEqual($list->count(), 0);
 
-		$coll->push(range(1, 5));
-		$this->assertEqual($coll->count(), 5);
+		$list->push(range(1, 5));
+		$this->assertEqual($list->count(), 5);
 
-		$coll->removeFromBeginning(3);
-		$this->assertEqual($coll->count(), 2);
-		$this->assertTrue($coll->hasAllOf(array(4, 5)));
+		$list->removeFromBeginning(3);
+		$this->assertEqual($list->count(), 2);
+		$this->assertTrue($list->hasAllOf(array(4, 5)));
 
-		$coll->removeFromEnd();
-		$this->assertEqual($coll->count(), 1);
-		$this->assertTrue($coll->hasAnyOf(4));
+		$list->removeFromEnd();
+		$this->assertEqual($list->count(), 1);
+		$this->assertTrue($list->hasAnyOf(4));
 
-		$coll->removeAll();
-		$this->assertEqual($coll->count(), 0);
+		$list->removeAll();
+		$this->assertEqual($list->count(), 0);
 
-		$coll->push(range(1, 6));
+		$list->push(range(1, 6));
 		$slice_tests = array(
 			array(0, null,  range(1, 6)),
 			array(0, 4,     range(1, 4)),
@@ -138,31 +138,31 @@ class ElggxListsTest extends ElggCoreUnitTest {
 		);
 		foreach ($slice_tests as $test) {
 			$expected = $test[2];
-			$returned = $coll->slice($test[0], $test[1]);
+			$returned = $list->slice($test[0], $test[1]);
 			$this->assertEqual(
 				$returned,
 				$expected,
 				"slice({$test[0]}, {$test[1]}) returned [" . implode(',', $returned) . "]");
 		}
 
-		$coll->moveAfter(2, 4);
-		$this->assertEqual($coll->slice(), array(1, 3, 4, 2, 5, 6));
+		$list->moveAfter(2, 4);
+		$this->assertEqual($list->slice(), array(1, 3, 4, 2, 5, 6));
 
-		$coll->moveBefore(5, 4);
-		$this->assertEqual($coll->slice(), array(1, 3, 5, 4, 2, 6));
+		$list->moveBefore(5, 4);
+		$this->assertEqual($list->slice(), array(1, 3, 5, 4, 2, 6));
 
-		$coll->moveBefore(5, 1);
-		$this->assertEqual($coll->slice(), array(5, 1, 3, 4, 2, 6));
+		$list->moveBefore(5, 1);
+		$this->assertEqual($list->slice(), array(5, 1, 3, 4, 2, 6));
 
-		$this->assertFalse($coll->moveAfter(4, 1));
+		$this->assertFalse($list->moveAfter(4, 1));
 
-		$coll->rearrange(array(3, 4, 2, 6), array(6, 4, 3, 2));
-		$this->assertEqual($coll->slice(), array(5, 1, 6, 4, 3, 2));
+		$list->rearrange(array(3, 4, 2, 6), array(6, 4, 3, 2));
+		$this->assertEqual($list->slice(), array(5, 1, 6, 4, 3, 2));
 
-		$coll->rearrange(array(5, 1, 6, 4, 3, 2), array(1, 5, 6, 4, 3, 2));
-		$this->assertEqual($coll->slice(), array(1, 5, 6, 4, 3, 2));
+		$list->rearrange(array(5, 1, 6, 4, 3, 2), array(1, 5, 6, 4, 3, 2));
+		$this->assertEqual($list->slice(), array(1, 5, 6, 4, 3, 2));
 
-		$coll->removeAll();
+		$list->removeAll();
 	}
 
 	/**
@@ -190,19 +190,19 @@ class ElggxListsTest extends ElggCoreUnitTest {
 
 		$user = elgg_get_logged_in_user_entity();
 		$name = 'testQueryModifier';
-		$coll = elggx_get_list($user, $name);
+		$list = elggx_get_list($user, $name);
 
-		$coll_guids = array($all_objs[2], $all_objs[4]);
-		$coll->push($coll_guids);
+		$list_guids = array($all_objs[2], $all_objs[4]);
+		$list->push($list_guids);
 
 		// selector
-		$mod = $coll->getQueryModifier();
+		$mod = $list->getQueryModifier();
 		$fetched_objs = elgg_get_entities($mod->getOptions(array(
 			'type' => 'object',
 			'subtype' => 'testQueryModifier',
 		)));
 		// selector returns most recent additions first by default
-		$expected = array_reverse($coll_guids);
+		$expected = array_reverse($list_guids);
 
 		$computed = $this->mapGuids($fetched_objs);
 		$this->assertEqual($expected, $computed);
@@ -218,7 +218,7 @@ class ElggxListsTest extends ElggCoreUnitTest {
 		$this->assertEqual($expected, $computed);
 
 		// sticky
-		$mod = $coll->getQueryModifier('sticky');
+		$mod = $list->getQueryModifier('sticky');
 		$fetched_objs = elgg_get_entities($mod->getOptions(array(
 			'type' => 'object',
 			'subtype' => 'testQueryModifier',
@@ -251,7 +251,7 @@ class ElggxListsTest extends ElggCoreUnitTest {
 		$this->assertEqual($expected, $computed);
 
 		// filter
-		$mod = $coll->getQueryModifier('filter');
+		$mod = $list->getQueryModifier('filter');
 		$fetched_objs = elgg_get_entities($mod->getOptions(array(
 			'type' => 'object',
 			'subtype' => 'testQueryModifier',
@@ -290,7 +290,7 @@ class ElggxListsTest extends ElggCoreUnitTest {
 		$computed = $this->mapGuids($fetched_objs);
 		$this->assertEqual($expected, $computed);
 
-		$coll->removeAll();
+		$list->removeAll();
 
 		foreach ($objs as $obj) {
 			$obj->delete();
